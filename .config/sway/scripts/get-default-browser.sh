f4aae547e4f43c5556dd2ef70351fe26811a3e16
Vyacheslav Vorobev <voyzvz@gmail.com>
2024-03-15 13:37:30 +0800

    MAIN: fix get default browser sh

--- .config/sway/scripts/get-default-browser.sh
+++ .config/sway/scripts/get-default-browser.sh
@@ -6,10 +6,14 @@ for p in ${XDG_DATA_DIRS//:/ }; do
     APP_PATH="$p/applications"
     if [ -d $APP_PATH ]; then
         BROWSER=$(find $APP_PATH -name $DEFAULT_BROWSER)
+        if [ -n "$BROWSER" ]; then
+            break
+        fi
     fi
 done
 
-RAW=$(grep -Rnw -m 1 $BROWSER -e 'Exec=')
+RAW=$(grep -R -m 1 $BROWSER -e 'Exec=')
+
 BROWSER_CMD=$(echo $RAW | cut -d '=' -f 2)
 BROWSER_CMD=$(echo $BROWSER_CMD | cut -d ' ' -f 1)
 
